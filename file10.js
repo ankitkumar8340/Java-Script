baea2a12ebd7871b10a12510ab642aad8d19e5fd
ankit <ankit@example.com>
2026-02-11 15:27:06 +0530

    updated

---
+++ file10.js
@@ -0,0 +1,35 @@
+// let a =10;
+// console.log(a)
+
+// let a = function greet(){
+//     console.log("Hello world")
+// }
+// console.log(a);
+// a()
+
+// let greet = () =>{
+//     console.log("Hello world");
+// }
+// greet()
+
+const add = (x, y) =>{
+    return x+y
+}
+let result = add(2,3);
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
