e7174c8dc6b9e0974d536b484b9301fb11dca687
ankit <ankit@example.com>
2026-02-11 15:57:35 +0530

    updated

---
+++ file12.js
@@ -0,0 +1,36 @@
+
+const student = {
+    name:"Ankit",
+    age:21
+}
+// console.log(student)
+// console.log(student.name)
+// console.log(student.age)
+
+// student.city = "NYC"
+// console.log(student)
+
+// student.city = "miami"
+// console.log(student)
+
+//spread operator
+const obj = {...student, city:"nyc"}
+console.log(obj);
+const obj1 = {...student, city:"delhi"}
+console.log(obj1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
