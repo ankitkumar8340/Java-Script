439c0134ee2d66bdfa9f6c21e2a6187dd6e72bd3
ankit <ankit@example.com>
2026-02-11 12:45:58 +0530

    updated

---
+++ file3.js
@@ -0,0 +1,37 @@
+let a =10
+let b = 4
+
+//arithematic operator
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+
+
+//Comparison operator
+
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a!=b)
+
+let x = "10"
+let y = 10
+console.log(x == y) //true
+console.log(x === y) //false
+
+//logical operator
+console.log(a>b && a > 20)  //false
+console.log(a>b || a > 20)  //true
+console.log(!a>b)  // false
+
+
+
+
+
+
+
+
---
+++ file5.js
@@ -0,0 +1,29 @@
+// let i =1;
+// while(i<=4){
+//     console.log(i)
+//     i++;
+// }
+
+// let i =1;
+// while(i<=4){
+   
+//     i++;
+//     console.log(i);
+//     if(i==3){
+//         break;
+//     }
+// }
+
+let v=4;
+for(i=1;i<=10;i++){
+    console.log(v*i)
+}
+
+
+
+
+5
+
+
+
+
