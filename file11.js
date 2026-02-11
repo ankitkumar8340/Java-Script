b9cdfdc56820637d26d930bf8ec05a7f3555493c
ankit <ankit@example.com>
2026-02-11 15:33:51 +0530

    updated

---
+++ file11.js
@@ -0,0 +1,28 @@
+// const add = (x, y)=>{
+//     return x+y;
+// }
+// const add = (x, y) => x+y;
+
+// const result = add(4,3);
+// console.log(result);
+
+
+const sqr = (a) => a*a 
+
+const result = sqr(4)
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
