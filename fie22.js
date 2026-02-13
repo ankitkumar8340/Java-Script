05dc8059d949a0f7b18cd1914a4a27773cfe510e
ankit <ankitkumarswe01@gmail.com>
2026-02-13 16:54:10 +0530

    updated

--- fie22.js
+++ fie22.js
@@ -1,7 +1,4 @@
 
-
-
-
 const products = [
   {
     id: 1,
---
+++ modular/file30.js
@@ -0,0 +1,38 @@
+import {add, sub, mul} from "./calc.js"
+import {name} from "./calc.js"
+import { products } from "./data.js";
+
+const result = add(4,3);
+const result1 = sub(4,3);
+const result2 = mul(4,3);
+// console.log(result)
+// console.log(result1)
+// console.log(result2)
+// console.log(name)
+debugger;//press F10 to continue
+products.map((data)=>{
+    console.table(data);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
