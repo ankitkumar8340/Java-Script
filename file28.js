0de845116b9f4444c687bd9ae17595206d88abf6
ankit <ankitkumarswe01@gmail.com>
2026-02-13 11:44:00 +0530

    updated

---
+++ file28.js
@@ -0,0 +1,13 @@
+// const names = ["john", "cathhy", "amy"]
+// names.forEach((name, index, arr)=>{
+//     console.log(arr[index]);
+// })
+
+
+const names = ["john", "cathy", "amy"];
+const score = [37, 87, 98];
+
+names.forEach((name, index)=>{
+    console.log(name, score[index])
+})
+
