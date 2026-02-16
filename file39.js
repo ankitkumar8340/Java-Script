c6697ed21d2175969366dd00a7b2ce17c1aa2303
ankit <ankitkumarswe01@gmail.com>
2026-02-16 14:12:24 +0530

    updated

---
+++ file39.js
@@ -0,0 +1,33 @@
+//hoisting
+
+// var name
+// console.log(name) // give undefined
+// var name="john"
+
+// let name
+// console.log(name)  //give error
+// let name="john"
+
+//function are fully hoisted
+greet()  
+
+// function greet(){
+//     console.log("Hello world");
+// }
+
+//it will give error
+const greet = ()=>{
+    console.log("Hello world")
+}
+
+
+
+
+
+
+
+
+
+
+
+
