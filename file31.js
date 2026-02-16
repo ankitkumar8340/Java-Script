f02e9483d4dfaf42e96800d430515e78dd90ac4f
ankit <ankitkumarswe01@gmail.com>
2026-02-16 13:25:07 +0530

    updated

---
+++ file31.js
@@ -0,0 +1,15 @@
+import Header from "./header.js"
+import Footer from "./footer.js"
+import Products from "./products.js"
+
+console.log(Header())
+console.log(".........................")
+console.table(Products())
+console.log(".........................")
+console.log(Footer())
+
+
+
+
+
+
