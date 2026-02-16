a3ee5a4537270a62a96f9dffe66190515e5041f2
ankit <ankitkumarswe01@gmail.com>
2026-02-16 13:22:26 +0530

    updated

---
+++ file30.js
@@ -0,0 +1,9 @@
+import add,{subtract} from "./calc.js"
+import {name} from "./calc.js"
+const result = add(4,5)
+console.log(result)
+const result1 = subtract(20,5)
+console.log(result1)
+console.log(name)
+
+
