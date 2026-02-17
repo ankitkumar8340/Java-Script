308629b09c384fa3cc6bedf1944b2dfdc6d9fe58
ankit <ankitkumarswe01@gmail.com>
2026-02-17 11:33:23 +0530

    updated

---
+++ file48.js
@@ -0,0 +1,22 @@
+const students=[
+    {id:1, name:"vansh", s1:true, s2:true, s3:false},
+    {id:1, name:"Ayush", s1:true, s2:true, s3:true},
+    {id:1, name:"Ankit", s1:true, s2:true, s3:true},
+    {id:1, name:"Anmol", s1:false, s2:true, s3:false}
+]
+
+// if id is 1 > vansh is absent
+
+
+
+
+
+
+
+
+
+
+
+
+
+
