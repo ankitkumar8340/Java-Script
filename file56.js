fbfeff32f4d355a0757f43198804320802fc1daf
ankit <ankitkumarswe01@gmail.com>
2026-02-17 14:35:08 +0530

    updated

---
+++ file56.js
@@ -0,0 +1,22 @@
+// const employees = ["john", "cathy", "Mike"];
+// for(let e in employees){
+//     // console.log(e)
+//     console.log(employees[e])
+// }
+
+const student = {
+    name:"john",
+    age:23
+}
+for( let s in student){
+    console.log(student[s])
+}
+
+
+
+
+
+
+
+
+
